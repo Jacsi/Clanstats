9adfb63ba49e53b646f878f2fcdee3f2b17390c4
Jacsi <96053932+Jacsi@users.noreply.github.com>
2021-12-13 13:30:12 +0100

    Add files via upload
    streamlit app to show clan stats an example

---
+++ app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 13 00:48:34 2021
+
+@author: JSiet
+"""
+
+import streamlit as st
+from multiapp import MultiApp
+from apps import data, clanstats # import your app modules here
+
+app = MultiApp()
+
+# Add all your application here
+app.add_app("Clan statistics", clanstats.app)
+app.add_app("Data", data.app)
+
+# The main app
+app.run()
